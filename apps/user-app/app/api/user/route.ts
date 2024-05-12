1c48e252897b0747caea3d03db2b65167af099f4
abbas110 <meerabbas110@gmail.com>
2024-05-12 12:42:53 +0530

    initial commit for the paytm full stack project

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,17 +1,27 @@
-import { getServerSession } from "next-auth"
-import { NextResponse } from "next/server";
-import { authOptions } from "../../lib/auth";
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { authOptions } from '../../lib/auth';
 
 export const GET = async () => {
+  try {
     const session = await getServerSession(authOptions);
     if (session.user) {
-        return NextResponse.json({
-            user: session.user
-        })
+      return NextResponse.json({
+        user: session.user,
+      });
     }
+  } catch (error) {
     return NextResponse.json({
-        message: "You are not logged in"
-    }, {
-        status: 403
-    })
-}
+      msg: 'you are not logged in',
+    });
+  }
+
+  return NextResponse.json(
+    {
+      message: 'You are not logged in',
+    },
+    {
+      status: 403,
+    }
+  );
+};
